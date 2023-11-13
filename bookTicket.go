4bb1da8b5983c2229475d6e2eb84a0948dce948d
CraigGdn <93987791+CraigGdn@users.noreply.github.com>
2023-11-13 01:30:53 +0000

    added additional files for ease and order (#12)
    Co-authored-by: Craig gooden <cg@Craigs-MacBook-Pro.local>

---
+++ bookTicket.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+
+	// create a map for a user
+	var userData = UserData {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		numberOfTickets: userTickets,
+	}
+	bookings = append(bookings, userData) //this is a slice
+	fmt.Printf("List of bookings is %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v Tickets are remaining for the %v\n", remainingTickets, conferenceName)
+}
